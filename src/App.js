e483949c1ca5f8b61d583680935e0282dab210a8
yaswanth <yash.topaz@gmail.com>
2021-11-15 18:29:58 +0530

    Added pet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 //  App is a react component => basic building block in react which uses functions and classes to create reusable components
+
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div",
     {},
-    React.createElement("h1", {}, "Adopt Me!")
+    [
+      React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
+      React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+      React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Greman Shepard" })
+    ]
   );
 };
 
