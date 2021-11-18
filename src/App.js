c8da2d5e60bf54dd34ef624000b4bc0f7a14aee5
yaswanth <yash.topaz@gmail.com>
2021-11-18 14:39:32 +0530

    Changed the code in components to jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //  App is a react component => basic building block in react which uses functions and classes to create reusable components
 
-import React from "react";
+// import React from "react"; //Babel automatically finds
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
@@ -8,26 +8,40 @@ import Pet from "./Pet";
 // "name" => node module from npm
 // 98% percent of times it is to have a separate file for every component
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Lunaaa",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+// breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sudo",
+//       animal: "Dog",
+//       breed: "German Shepard",
+//     }),
+//   ]);
+// };
+
+//jsx
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Lunaaa",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Greman Shepard",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" animal="Dog" breed="German Shepard" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root")); // old way
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,23 @@
-import React from "react";
+// import React from "react";
 
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.breed),
+//   ]);
+// };
+
+//using jsx
+// the above code is what below code will get transpiled into
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
 };
 
 export default Pet;
